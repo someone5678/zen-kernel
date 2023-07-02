4890699696c18f19052da4655fb3436dfa54bcfa
someone5678 <nemui3353@gmail.com>
2023-07-03 00:25:36 +0900

    Revert "HID: hidraw: fix data race on device refcount"
    This reverts commit 05b47034e2488c2924e5c032e20a1979d012b5b5.

--- drivers/hid/hidraw.c
+++ drivers/hid/hidraw.c
@@ -272,12 +272,7 @@ static int hidraw_open(struct inode *inode, struct file *file)
 		goto out;
 	}
 
-	/*
-	 * Technically not writing to the hidraw_table but a write lock is
-	 * required to protect the device refcount. This is symmetrical to
-	 * hidraw_release().
-	 */
-	down_write(&minors_rwsem);
+	down_read(&minors_rwsem);
 	if (!hidraw_table[minor] || !hidraw_table[minor]->exist) {
 		err = -ENODEV;
 		goto out_unlock;
@@ -306,7 +301,7 @@ static int hidraw_open(struct inode *inode, struct file *file)
 	spin_unlock_irqrestore(&hidraw_table[minor]->list_lock, flags);
 	file->private_data = list;
 out_unlock:
-	up_write(&minors_rwsem);
+	up_read(&minors_rwsem);
 out:
 	if (err < 0)
 		kfree(list);
